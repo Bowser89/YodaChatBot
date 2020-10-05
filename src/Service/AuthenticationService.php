1941eb0ec223d0b3d9b179b0f704ab43fdd8262f
Augusto Lamona <augusto.l@elearnsecurity.com>
2020-10-05 16:11:56 +0200

    bootstrapped application

---
+++ src/Service/AuthenticationService.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the eLearnSecurity website project.
+ *
+ * Created on 05/10/2020
+ *
+ * @copyright Caendra Inc.
+ */
+
+namespace App\Service;
+
+/**
+ * AuthenticationService.
+ */
+class AuthenticationService
+{
+
+}
