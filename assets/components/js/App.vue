9bc5b056077ebe1357ee954321ea817017207590
Augusto Lamona <augusto.l@elearnsecurity.com>
2020-10-29 23:14:17 +0100

    updated frontend vue

--- assets/components/js/App.vue
+++ assets/components/js/App.vue
@@ -1,57 +1,77 @@
 <template>
     <div>
         <h2>Talk with Yoda!</h2>
-        <div id="conversation"></div>
-        <p v-if="isWriting">Yoda is writing . . .</p>
-        <form v-on:submit.prevent="sendMessage" ><br>
-            <input type="text" id="messageyoda" placeholder="Ask something to Yoda!" v-model="form.message" required>
+        <div id="conversation">
+            <div v-for="messageItem in messages">
+                <div v-if="messageItem.source === 'Human'">
+                    You:
+                </div>
+                <div v-else>
+                    YodaBot Says:
+                </div>
+                <div v-if="messageItem.titlePhrase">
+                    {{messageItem.titlePhrase}}
+                    <div v-for="message in messageItem.messages ">
+                        <li>{{ message }}</li>
+                    </div>
+                </div>
+                <div v-else>
+                    <div v-for="message in messageItem.messages ">
+                        {{ message }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p v-if="isWriting">Yoda is looking for knowledge . . .</p>
+        <form v-on:submit.prevent="sendMessage"><br>
+            <input type="text" placeholder="Ask something to the real Yoda!" v-model="form.message" required>
             <button class="btn btn-primary">Submit</button>
         </form>
+        <button class="btn btn-primary" v-on:click="clearHistory">Clear History</button>
+
     </div>
 </template>
 
 <script>
     export default {
+        props: {
+            previousConversation: {
+                type : Array,
+                default: []
+            }
+        },
         data: function() {
             return{
                 form: {
                     message: ''
                 },
-                isWriting: false
+                isWriting: false,
+                messages: this.previousConversation,
             }
         },
         methods:{
             sendMessage() {
-                $('#conversation').append('Me: '+this.form.message+'<br><br>');
+                var userQuestion = {'messages' : [this.form.message], 'source' : 'Human', 'titlePhrase' : null};
+                this.appendToConversation(userQuestion);
                 this.isWriting = true;
 
                 axios.post('/api/send_message', this.form)
                     .then((response) => {
+                        this.form.message = '';
                         this.isWriting = false;
-                        this.appendToConversation(response.data);
+                        this.appendToConversation(response.data.message);
                     })
                     .catch((error) => {
-                        // error.response.status Check status code
+                        this.form.message = '';
                     }).finally(() => {
-                    //Perform action in always
+                    this.form.message = '';
                 });
             },
             appendToConversation(data) {
-                var conversation;
-                conversation = '<b>'+data.source+' Says:</b> ';
-
-                if (data.titlePhrase != null) {
-                    conversation += data.titlePhrase+'</br>';
-                    for (var i = 0; i < data.messages.length; i++) {
-                        conversation += '<li>'+data.messages[i]+'</li></br>';
-                    }
-                } else {
-                    for (var i = 0; i < data.messages.length; i++) {
-                        conversation += data.messages[i]+'</br>';
-                    }
-                }
-
-                $('#conversation').append(conversation);
+                this.messages.push({'messages' : data.messages, 'source' : data.source , 'titlePhrase' : data.titlePhrase });
+            },
+            clearHistory() {
+                this.messages = [];
             }
         }
     }
