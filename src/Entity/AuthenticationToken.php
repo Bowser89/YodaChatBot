3aeb1320c0b5404d2fafeb7f1e3b360c55052ede
Augusto Lamona <augusto.l@elearnsecurity.com>
2020-10-29 15:36:58 +0100

    added token management classes

---
+++ src/Entity/AuthenticationToken.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Entity;
+
+use DateTime;
+
+/**
+ * AuthenticationToken.
+ */
+class AuthenticationToken
+{
+    /**
+     * Number of seconds threshold where the token has to be refreshed.
+     */
+    const TIMESTAMP_REFRESH_THRESHOLD = 180;
+
+    /**
+     * The array key where the chatbot api url is associated.
+     */
+    const CHATBOT_API_KEY = "chatbot";
+
+    /**
+     * The access token value.
+     *
+     * @var string
+     */
+    private $accessToken;
+
+    /**
+     * The timestamp date until the token is valid.
+     *
+     * @var int
+     */
+    private $tokenExpiration;
+
+    /**
+     * The token TTL in seconds.
+     *
+     * @var int
+     */
+    private $tokenExpirationRemainingTime;
+
+    /**
+     * An array containing API useful urls.
+     *
+     * @var array
+     */
+    private $apiInformation;
+
+    /**
+     * Check if user is identified.
+     *
+     * @var bool
+     */
+    private $isUserIdentified;
+
+    public function __construct(
+        string $accessToken,
+        int $tokenExpiration,
+        int $tokenExpirationRemainingTime,
+        array $apiInformation,
+        bool $isUserIdentified
+    )
+    {
+        $this->accessToken                  = $accessToken;
+        $this->tokenExpiration              = $tokenExpiration;
+        $this->tokenExpirationRemainingTime = $tokenExpirationRemainingTime;
+        $this->apiInformation               = $apiInformation;
+        $this->isUserIdentified             = $isUserIdentified;
+    }
+
+    /**
+     * Gets the access token value.
+     */
+    public function getAccessToken(): string
+    {
+        return $this->accessToken;
+    }
+
+    /**
+     * Gets the timestamp date until the token is valid.
+     */
+    public function getTokenExpiration(): int
+    {
+        return $this->tokenExpiration;
+    }
+
+    /**
+     * Gets the token expiration.
+     */
+    public function getTokenExpirationRemainingTime(): int
+    {
+        return $this->tokenExpirationRemainingTime;
+    }
+
+    /**
+     * Gets the API
+     */
+    public function getApiInformation(): array
+    {
+        return $this->apiInformation;
+    }
+
+    /**
+     * Sets the access token value.
+     */
+    public function setAccessToken(string $accessToken): self
+    {
+        $this->accessToken = $accessToken;
+
+        return $this;
+    }
+
+    /**
+     * Sets the timestamp date until the token is valid.
+     */
+    public function setTokenExpiration(int $tokenExpiration): self
+    {
+        $this->tokenExpiration = $tokenExpiration;
+
+        return $this;
+    }
+
+    /**
+     * Sets the token expiration.
+     */
+    public function setTokenExpirationRemainingTime(int $tokenExpirationRemainingTime): self
+    {
+        $this->tokenExpirationRemainingTime = $tokenExpirationRemainingTime;
+
+        return $this;
+    }
+
+    /**
+     * Checks if user is identified.
+     */
+    public function isUserIdentified(): bool
+    {
+        return $this->isUserIdentified;
+    }
+
+    /**
+     * Checks if the token has to be refreshed.
+     */
+    public function hasToBeRefreshed(): bool
+    {
+        $timeStampDifference = $this->tokenExpiration - time();
+
+        return 0 < $timeStampDifference && self::TIMESTAMP_REFRESH_THRESHOLD > $timeStampDifference;
+    }
+
+    /**
+     * Checks if token is expired.
+     */
+    public function isExpired(): bool
+    {
+        $timeStampDifference = $this->tokenExpiration - time();
+
+        return 0 > $timeStampDifference;
+    }
+
+    /**
+     * Gets the chatbot api url.
+     */
+    public function getChatBotApiUrl(): string
+    {
+        return $this->apiInformation[self::CHATBOT_API_KEY];
+    }
+
+}
---
+++ src/Entity/SessionToken.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entity;
+
+/**
+ * SessionToken.
+ */
+class SessionToken
+{
+    /**
+     * The session id.
+     *
+     * @var string
+     */
+    private $sessionId;
+
+    /**
+     * The session token value.
+     *
+     * @var string
+     */
+    private $sessionToken;
+
+    /**
+     * The constructor method.
+     */
+    public function __construct(string $sessionId, string $sessionToken)
+    {
+        $this->sessionId    = $sessionId;
+        $this->sessionToken = $sessionToken;
+    }
+
+    /**
+     * Gets the session token id.
+     */
+    public function getSessionId(): string
+    {
+        return $this->sessionId;
+    }
+
+    /**
+     * Gets the session token.
+     */
+    public function getSessionToken(): string
+    {
+        return $this->sessionToken;
+    }
+}
